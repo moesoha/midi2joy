5863680e9a2ce2a26c54ce7d710217beab1478fe
Soha Jin <soha@lohu.info>
2019-01-26 01:39:50 +0800

    add x86 lib to support 32 bit windows

--- virtual-xbox-interface/build.rs
+++ virtual-xbox-interface/build.rs
@@ -1,6 +1,12 @@
+use std::env;
+use std::path::Path;
+
 fn main() {
-	let lib_dir=concat!(env!("CARGO_MANIFEST_DIR"),"/lib");
+	let target=env::var("TARGET").unwrap();
+	let lib_dir=format!("{}/lib/{}", env!("CARGO_MANIFEST_DIR"), target);
+	if !Path::new(&lib_dir).exists() {
+		panic!(format!("Target \"{}\" is not supported.", target));
+	}
 	println!("cargo:rustc-link-lib=static=vXboxInterface");
-	println!("cargo:rustc-link-search={}",lib_dir);
-	// TODO: copy dll to dest
+	println!("cargo:rustc-link-search={}", lib_dir);
 }
