4af8acdb712a1d6a87396b2a402df206742e5104
Soha Jin <soha@lohu.info>
2019-01-26 01:15:53 +0800

    Remove some duplicate comments

--- virtual-xbox-interface/src/lib.rs
+++ virtual-xbox-interface/src/lib.rs
@@ -3,43 +3,36 @@ use winapi::{shared::{minwindef::{BOOL,UCHAR,UINT,INT,BYTE},ntdef::{SHORT}},um::
 #[allow(non_camel_case_types)]
 #[link(name="vXboxInterface", kind="static")]
 extern {
-	// Status
 	fn isVBusExists()->BOOL;
 	fn GetNumEmptyBusSlots(nSlots: *mut UCHAR)->BOOL;
 	fn isControllerExists(UserIndex: UINT)->BOOL;
 	fn isControllerOwned(UserIndex: UINT)->BOOL;
-
-	// Virtual device Plug-In/Unplug
 	fn PlugIn(UserIndex: UINT)->BOOL;
 	fn UnPlug(UserIndex: UINT)->BOOL;
 	fn UnPlugForce(UserIndex: UINT)->BOOL;
-
-	// Data Transfer (Data to the device)
 	fn SetBtnA(UserIndex: UINT, Press: BOOL)->BOOL;
 	fn SetBtnB(UserIndex: UINT, Press: BOOL)->BOOL;
 	fn SetBtnX(UserIndex: UINT, Press: BOOL)->BOOL;
 	fn SetBtnY(UserIndex: UINT, Press: BOOL)->BOOL;
 	fn SetBtnStart(UserIndex: UINT, Press: BOOL)->BOOL;
 	fn SetBtnBack(UserIndex: UINT, Press: BOOL)->BOOL;
-	fn SetBtnLT(UserIndex: UINT, Press: BOOL)->BOOL; // Left Thumb/Stick
-	fn SetBtnRT(UserIndex: UINT, Press: BOOL)->BOOL; // Right Thumb/Stick
-	fn SetBtnLB(UserIndex: UINT, Press: BOOL)->BOOL; // Left Bumper
-	fn SetBtnRB(UserIndex: UINT, Press: BOOL)->BOOL; // Right Bumper
-	fn SetBtnGD(UserIndex: UINT, Press: BOOL)->BOOL; // Guide Button (Undocumanted)
-	fn SetTriggerL(UserIndex: UINT, Value: BYTE)->BOOL; // Left Trigger
- 	fn SetTriggerR(UserIndex: UINT, Value: BYTE)->BOOL; // Right Trigger
-	fn SetAxisX(UserIndex: UINT, Value: SHORT)->BOOL; // Left Stick X
-	fn SetAxisY(UserIndex: UINT, Value: SHORT)->BOOL; // Left Stick Y
-	fn SetAxisRx(UserIndex: UINT, Value: SHORT)->BOOL; // Right Stick X
-	fn SetAxisRy(UserIndex: UINT, Value: SHORT)->BOOL; // Right Stick Y
+	fn SetBtnLT(UserIndex: UINT, Press: BOOL)->BOOL;
+	fn SetBtnRT(UserIndex: UINT, Press: BOOL)->BOOL;
+	fn SetBtnLB(UserIndex: UINT, Press: BOOL)->BOOL;
+	fn SetBtnRB(UserIndex: UINT, Press: BOOL)->BOOL;
+	fn SetBtnGD(UserIndex: UINT, Press: BOOL)->BOOL;
+	fn SetTriggerL(UserIndex: UINT, Value: BYTE)->BOOL;
+ 	fn SetTriggerR(UserIndex: UINT, Value: BYTE)->BOOL;
+	fn SetAxisX(UserIndex: UINT, Value: SHORT)->BOOL;
+	fn SetAxisY(UserIndex: UINT, Value: SHORT)->BOOL;
+	fn SetAxisRx(UserIndex: UINT, Value: SHORT)->BOOL;
+	fn SetAxisRy(UserIndex: UINT, Value: SHORT)->BOOL;
 	fn SetDpadUp(UserIndex: UINT)->BOOL;
 	fn SetDpadRight(UserIndex: UINT)->BOOL;
 	fn SetDpadDown(UserIndex: UINT)->BOOL;
 	fn SetDpadLeft(UserIndex: UINT)->BOOL;
 	fn SetDpadOff(UserIndex: UINT)->BOOL;
 	fn SetDpad(UserIndex: UINT, Value: INT)->BOOL;
-
-	// Data Transfer (Feedback from the device)
 	fn GetLedNumber(UserIndex: UINT, PpLed: *mut BYTE)->BOOL;
 	fn GetVibration(UserIndex: UINT, pVib: *mut XINPUT_VIBRATION)->BOOL;
 }
